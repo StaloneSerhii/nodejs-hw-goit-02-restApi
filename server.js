fa370c241c5724e122b7118a114447f01bf909fb
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-05-10 08:30:30 +0300

    connect fc

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 app.listen(3011, () => {
   console.log("Server running. Use our API on port: 3011");
 });
+
+const DB_HOST =
+  "mongodb+srv://SergiyAgent:SGALZWqreRGNTh8l@cluster0.6sly2yr.mongodb.net/contactBD?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error);
+    process.error();
+  });
